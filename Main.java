2973f87df08100b4c0aadcd88388baa2a885a548
juansimonEAFIT <jsospinam@eafit.edu.co>
2024-05-10 20:57:07 -0500

    Este es el codigo modificado

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Hola profesor :) ") ;
+	System.out.println("Hola profe") ;
+        printMessage ();
+    }
+
+    public static void printMessage() {
+        System.out.println("Tarea 3 por Juan Simón Ospina Martínez");
     }
 }
+
